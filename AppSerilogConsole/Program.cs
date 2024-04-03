96584675343237051d7c8769872dce53906f5bce
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2024-04-02 21:56:00 -0300

    feat: criação aplicação console com serilog

---
+++ AppSerilogConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using Serilog;
+
+class Program
+{
+  static async Task Main()
+  {
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("logs/my-logs.txt", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+
+    Log.Information("Meu log!");
+
+    int a = 10, b = 0;
+    try
+    {
+      Log.Debug("Dividindo {A} by {B}", a, b);
+      Console.WriteLine(a / b);
+    }
+    catch (Exception ex)
+    {
+      Log.Error(ex, "Algo deu errado");
+    }
+    finally
+    {
+      await Log.CloseAndFlushAsync();
+    }
+  }
+}
